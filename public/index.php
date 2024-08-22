1313680fa93357a5f9351bd0fad842e5079d8de3
kasunmendis7 <tkomendis@gmail.com>
2024-08-22 15:56:45 +0530

    added technician sign up page

--- public/index.php
+++ public/index.php
@@ -5,11 +5,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use app\core\Application;
 use app\controllers\SiteController;
 use app\controllers\CustomerController;
+use app\controllers\TechnicianController;
 
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/customer-sign-up', [CustomerController::class, 'customerSignUp']);
+$app->router->get('/technician-sign-up', [TechnicianController::class, 'technicianSignUp']);
+
 $app->run();
 
 
