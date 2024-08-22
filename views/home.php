6ebc2e528fdac479906418999876b9665724d29a
sheanemario <sheanemario7@gmail.com>
2024-08-22 15:38:28 +0530

    added home page layout

--- views/home.php
+++ views/home.php
@@ -1,5 +1,32 @@
 <?php
-
 ?>
 
-<h1>Hello Home</h1>
+<section class="section-1">
+    <div class="box-1">
+        <div class="box-1-container">
+            <div class="box-1-navigate">
+                <div class="item-1">
+                    <button class="active item-btn" onclick="toggleVisibility()">Customer</button>
+                </div>
+                <div class="item-2">
+                    <button class="item-btn" onclick="toggleVisibility()">Technicians</button>
+                </div>
+            </div>
+            <div class="box-1-content">
+                <h2 class="box-1-title">Find technician and get fixed</h2>
+                <p class="box-1-desc">
+                    Find trustworthy technician when your vehicle breaks down
+                </p>
+                <button class="box-1-btn" type="button">Find Technician</button>
+            </div>
+            <div class="box-1-content hidden">
+                <h2 class="box-1-title">Provide services to customers</h2>
+                <p class="box-1-desc">
+                    Provide service to customers who are in need of your service
+                </p>
+                <button class="box-1-btn" type="button">Find Customers</button>
+            </div>
+        </div>
+    </div>
+    <div class="box-2"></div>
+</section>
