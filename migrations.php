00b1f997b263d4cbfefddf95008c3f0349d66748
kasunmendis7 <tkomendis@gmail.com>
2024-11-02 21:24:00 +0530

    database connection and migrations

--- migrations.php
+++ migrations.php
@@ -5,8 +5,8 @@ use app\controllers\SiteController;
 use app\controllers\CustomerController;
 use app\controllers\TechnicianController;
 
-require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+require_once __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $config = [
@@ -17,7 +17,7 @@ $config = [
     ]
 ];
 
-$app = new Application(dirname(__DIR__), $config);
+$app = new Application(__DIR__, $config);
 
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/customer-sign-up', [CustomerController::class, 'customerSignUp']);
@@ -26,5 +26,5 @@ $app->router->post('/technician-sign-up', [TechnicianController::class, 'technic
 $app->router->get('/technician-landing', [TechnicianController::class, 'technicianLanding']);
 $app->router->get('/technician-add-new-post', [TechnicianController::class, 'technicianAddNewPost']);
 $app->router->get('/technician-home', [TechnicianController::class, 'technicianHome']);
-$app->run();
 
+$app->db->applyMigrations();
