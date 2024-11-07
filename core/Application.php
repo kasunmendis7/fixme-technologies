fa014688ae2477fecd0b99e1ac2c677c767baad9
sheanemario <sheanemario7@gmail.com>
2024-11-07 16:11:06 +0530

    implemented customer login

--- core/Application.php
+++ core/Application.php
@@ -7,23 +7,55 @@ class Application
 
     public static string $ROOT_DIR;
     public static Application $app;
+    public string $customerClass;
     public Router $router;
     public Request $request;
     public Response $response;
     public Session $session;
     public Database $db;
     public Controller $controller;
+    public ?DbModel $customer;
 
     public function __construct($rootPath, array $config)
     {
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
+        $this->customerClass = $config['customerClass'];
         $this->request = new Request();
         $this->response = new Response();
         $this->session = new Session();
         $this->router = new Router($this->request, $this->response);
 
         $this->db = new Database($config['db']);
+
+        $primaryValueCustomer = $this->session->get('customer');
+        if ($primaryValueCustomer) {
+            $customerInstance = new $this->customerClass;
+            $primaryKey = $customerInstance->primaryKey();
+            $this->customer = $customerInstance->findOne([$primaryKey => $primaryValueCustomer]);
+        } else {
+            $this->customer = null;
+        }
+    }
+
+    public function loginCustomer(DbModel $customer)
+    {
+        $this->customer = $customer;
+        $primaryKey = $customer->primaryKey();
+        $primaryValue = $customer->{$primaryKey};
+        $this->session->set('customer', $primaryValue);
+        return true;
+    }
+
+    public function logoutCustomer()
+    {
+        $this->customer = null;
+        $this->session->remove('customer');
+    }
+
+    public static function isGuestCustomer()
+    {
+        return !self::$app->customer;
     }
 
     public function run()
