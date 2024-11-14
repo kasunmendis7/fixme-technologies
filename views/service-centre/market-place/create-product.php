4779c59d4732ad4b00d0ea53d3f2a77d264d198a
Pulasthi Abishake <pulasthiabishake06@gmail.com>
2024-11-14 21:47:38 +0100

    making simple confi/creating product model

--- views/service-centre/market-place/create-product.php
+++ views/service-centre/market-place/create-product.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Technician Dashboard</title>
+    <title>Create Product</title>
     <link rel="stylesheet" href="/css/technician/technician-community.css">
     <link rel="stylesheet" href="/css/technician/overlay.css">
 
