58989629d9ab418e9369b096f1e7135e1af99b84
kasunmendis7 <tkomendis@gmail.com>
2024-11-21 16:03:06 +0530

    added admin retrieve and delete functionality

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -2,14 +2,48 @@
 
 namespace app\controllers;
 
+use app\core\Application;
 use app\core\Controller;
+use app\core\Request;
+use app\models\Admin;
+use app\models\Customer;
 
 class AdminController extends Controller
 {
-    public function dashboard()
+    public function adminDashboard()
     {
         // Render the admin dashboard view
         $this->setLayout('auth');
-        return $this->render('admin/admin-dashboard');
+        return $this->render('/admin/admin-dashboard');
     }
+
+    public function customers()
+    {
+        // Fetch all customers records
+        $customers = Admin::findAllCustomers();
+        // Render the all the customer in the database
+        $this->setLayout('auth');
+        return $this->render('/admin/customers', ['customers' => $customers]);
+
+    }
+
+    public function deleteCustomer(Request $request)
+    {
+        $data = $request->getBody(); // Assuming this already returns an array
+        if (isset($data['cus_id'])) {
+            $cus_id = $data['cus_id'];
+
+            // Call the model function to delete the customer
+            $result = Admin::deleteCustomerById($cus_id);
+
+            if ($result) {
+                echo json_encode(['status' => 'success']);
+            } else {
+                echo json_encode(['status' => 'error', 'message' => 'Failed to delete customer']);
+            }
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'Invalid customer ID']);
+        }
+    }
+
 }
