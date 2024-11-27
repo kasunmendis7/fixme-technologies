edaaaf7286beb8ed23e6e7e83418e4e37fd86c17
Abhishke391 <pulasthiabishake06@gmail.com>
2024-11-27 22:27:06 +0530

    creating ui components

--- views/select-user-login.php
+++ views/select-user-login.php
@@ -12,16 +12,11 @@
     <!-- Left Column -->
     <div class="container-left">
 
-<!--        <div class="col-left-container">-->
-<!--            <div class="branding-container">-->
-<!--                <div>-->
-<!--                    <a href="/" class="brand-name">-->
-<!--                        <span class="brand-button">FixMe</span>-->
-<!--                    </a>-->
-<!--                </div>-->
-<!--                <p class="tagline">Expert Car Care, Anytime, Anywhere.</p>-->
-<!--            </div>-->
-<!--        </div>-->
+        <div class="logo-container">
+            <a href="/">
+            <img style="width: 600px;" src="/assets/logo/fixme-logo.png">
+            </a>
+        </div>
 
     </div>
 
--- views/select-user-sign-up.php
+++ views/select-user-sign-up.php
@@ -8,39 +8,55 @@
 </head>
 <body>
 <section>
-    <div class="container">
-        <div class="card" onmouseover="hoverEffect(this)" onmouseout="removeHoverEffect(this)">
-            <div class="icon">
-                <img src="assets/user_avatar.png" alt="Customer icon">
-            </div>
-            <h3 class="heading">Customer</h3>
-            <p class="paragraph">
-                Sign Up as a customer to find the best technicians in the nearby area.
-            </p>
-            <a href="/customer-sign-up" class="button">Sign Up</a>
-        </div>
 
-        <div class="card" onmouseover="hoverEffect(this)" onmouseout="removeHoverEffect(this)">
-            <div class="icon">
-                <img src="assets/select-user-technician.png" alt="guider icon">
-            </div>
-            <h3 class="heading">Technician</h3>
-            <p class="paragraph">
-                Sign Up as a Technician and find customers.
-            </p>
-            <a href="/technician-sign-up" class="button">Sign Up</a>
-        </div>
-        <div class="card" onmouseover="hoverEffect(this)" onmouseout="removeHoverEffect(this)">
-            <div class="icon">
-                <img src="assets/select-user-service-centre.png" alt="guider icon">
-            </div>
-            <h3 class="heading">Service Center</h3>
-            <p class="paragraph">
-                Sign Up as a Service Center and find customers.
-            </p>
-            <a href="/service-centre-sign-up" class="button">Sign Up</a>
+
+    <div class="container-left">
+
+        <div class="logo-container">
+            <a href="/">
+                <img style="width: 600px;" src="/assets/logo/fixme-logo.png">
+            </a>
         </div>
     </div>
+
+    <div class="container-right">
+        <div class="container">
+            <div class="card" onmouseover="hoverEffect(this)" onmouseout="removeHoverEffect(this)">
+                <div class="icon">
+                    <img src="assets/user_avatar.png" alt="Customer icon">
+                </div>
+                <h3 class="heading">Customer</h3>
+                <p class="paragraph">
+                    Sign up as a customer to find the best technicians in the nearby area.
+                </p>
+                <a href="/customer-sign-up" class="button">Sign up</a>
+            </div>
+
+            <div class="card" onmouseover="hoverEffect(this)" onmouseout="removeHoverEffect(this)">
+                <div class="icon">
+                    <img src="assets/select-user-technician.png" alt="Technician icon">
+                </div>
+                <h3 class="heading">Technician</h3>
+                <p class="paragraph">
+                    Sign up as a Technician and find customers.
+                </p>
+                <a href="/technician-sign-up" class="button">Sign up</a>
+            </div>
+
+            <div class="card" onmouseover="hoverEffect(this)" onmouseout="removeHoverEffect(this)">
+                <div class="icon">
+                    <img src="assets/select-user-service-centre.png" alt="Service Center icon">
+                </div>
+                <h3 class="heading">Service Center</h3>
+                <p class="paragraph">
+                    Sign up as a Service Center and find customers.
+                </p>
+                <a href="/service-centre-sign-up" class="button">Sign up</a>
+            </div>
+        </div>
+    </div>
+
+
 
 </section>
 
