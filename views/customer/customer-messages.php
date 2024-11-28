3b6be6375e411861985e35f765da84e4092abc96
sheanemario <sheanemario7@gmail.com>
2024-11-28 13:35:01 +0530

    Implement customer messages pane

---
+++ views/customer/customer-messages.php
@@ -0,0 +1,426 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Customer Messages</title>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <link rel="stylesheet" href="/css/customer/customer-messages.css">
+    <link rel="stylesheet" href="/css/customer/overlay.css">
+
+</head>
+<body>
+<?php
+include_once 'components/sidebar.php';
+include_once 'components/header.php';
+?>
+<div class="container">
+    <!-- ========= left side ========== -->
+    <div class="left-side">
+        <div class="header">
+            <div class="user-imgBx">
+                <img src="assets/technician-dashboard/customer02.jpg" alt="">
+            </div>
+
+            <ul class="nav-icons">
+                <li>
+                    <ion-icon name="chatbox"></ion-icon>
+                </li>
+                <li>
+                    <ion-icon name="ellipsis-vertical"></ion-icon>
+                </li>
+            </ul>
+        </div>
+
+        <div class="search-chat">
+            <input type="text" placeholder="Search or start new chat">
+            <ion-icon name="search-outline"></ion-icon>
+        </div>
+
+        <div class="chat-list">
+            <div class="chat active">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer02.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Sheane Mario</h4>
+                        <span class="time">10:57</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            How to Make WhatsApp clone using html and css only
+                        </p>
+                        <b class="num"></b>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer03.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Nimal Rathinarasa</h4>
+                        <span class="time">09:25</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            Hi, i found you on fixme
+                        </p>
+                        <b class="num unread">1</b>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer02.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Pulasthi Abhishek</h4>
+                        <span class="time">09:25</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            Hi, i found you on fixme also i found you on facebook
+                        </p>
+                        <b class="num unread">3</b>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer01.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Kasun Mendis</h4>
+                        <span class="time">09:25</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            Hi, i found you on fixme
+                        </p>
+                        <b class="num unread">2</b>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer02.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Sheane Mario</h4>
+                        <span class="time">05:25</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            Hi, i found you on fixme
+                        </p>
+                        <b class="num"></b>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer02.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Oshada Mendis</h4>
+                        <span class="time">05:25</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            Hi, i found you on fixme
+                        </p>
+                        <b class="num"></b>
+                    </div>
+                </div>
+            </div>
+
+            <!-- repeat last three 2X -->
+            <div class="chat">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer02.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Kasun Mendis</h4>
+                        <span class="time">09:25</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            Hi, i found you on fixme
+                        </p>
+                        <b class="num"></b>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer04.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Sheane Mario</h4>
+                        <span class="time">05:25</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            Hi, i found you on fixme
+                        </p>
+                        <b class="num"></b>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer01.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Oshada Mendis</h4>
+                        <span class="time">05:25</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            Hi, i found you on fixme
+                        </p>
+                        <b class="num"></b>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer03.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Kasun Mendis</h4>
+                        <span class="time">09:25</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            Hi, i found you on fixme
+                        </p>
+                        <b class="num"></b>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer01.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Sheane Mario</h4>
+                        <span class="time">05:25</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            Hi, i found you on fixme
+                        </p>
+                        <b class="num"></b>
+                    </div>
+                </div>
+            </div>
+
+            <div class="chat">
+                <div class="imgBx">
+                    <img src="assets/technician-dashboard/customer02.jpg" alt="">
+                </div>
+
+                <div class="details">
+                    <div class="head">
+                        <h4 class="name">Oshada Mendis</h4>
+                        <span class="time">05:25</span>
+                    </div>
+
+                    <div class="msgs">
+                        <p class="msg">
+                            Hi, i found you on fixme
+                        </p>
+                        <b class="num"></b>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ========= right side ========== -->
+    <div class="right-side">
+        <div class="header">
+            <div class="user-details">
+                <div class="user-imgBx">
+                    <img src="assets/technician-dashboard/customer03.jpg" alt="">
+                </div>
+
+                <h4>Sheane Mario <br>
+                    <span>Online</span>
+                </h4>
+            </div>
+
+            <ul class="nav-icons">
+                <li>
+                    <ion-icon name="search-outline"></ion-icon>
+                </li>
+                <li>
+                    <ion-icon name="ellipsis-vertical"></ion-icon>
+                </li>
+            </ul>
+        </div>
+
+        <div class="chatBx">
+            <div class="msg msg-me">
+                <p>Hi <br> <span>12:15</span></p>
+            </div>
+            <div class="msg msg-frnd">
+                <p>Hello <br> <span>12:15</span></p>
+            </div>
+
+            <!-- repeat 8X  -->
+            <div class="msg msg-me">
+                <p>I have an issue in my engine. I am now at Mount Lavinia will you be able to come to my location. <br>
+                    <span>12:15</span>
+                </p>
+            </div>
+            <div class="msg msg-frnd">
+                <p>Ok, I will come to your location please keep your GPS(location) ON in your phone. <br>
+                    <span>12:15</span></p>
+            </div>
+
+            <div class="msg msg-me">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto veritatis laborum deserunt error
+                    inventore? <br>
+                    <span>12:15</span>
+                </p>
+            </div>
+            <div class="msg msg-frnd">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+                    <span>12:15</span>
+                </p>
+            </div>
+
+            <div class="msg msg-me">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia placeat modi praesentium
+                    exercitationem reiciendis repudiandae velit eum harum maxime voluptatum.<br>
+                    <span>12:15</span>
+                </p>
+            </div>
+            <div class="msg msg-frnd">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, accusamus recusandae iusto
+                    inventore esse quae. <br>
+                    <span>12:15</span>
+                </p>
+            </div>
+
+            <div class="msg msg-me">
+                <p>Ok <br> <span>12:15</span></p>
+            </div>
+            <div class="msg msg-frnd">
+                <p>Thank You <br> <span>12:15</span></p>
+            </div>
+
+            <div class="msg msg-me">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus, tempora. <br>
+                    <span>12:15</span>
+                </p>
+            </div>
+            <div class="msg msg-frnd">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br> <span>12:15</span></p>
+            </div>
+
+            <div class="msg msg-me">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt error, similique voluptatem
+                    mollitia illo totam? <br>
+                    <span>12:15</span>
+                </p>
+            </div>
+            <div class="msg msg-frnd">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptate ipsam blanditiis! <br>
+                    <span>12:15</span>
+                </p>
+            </div>
+
+            <div class="msg msg-me">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia placeat modi praesentium
+                    exercitationem reiciendis repudiandae velit eum harum maxime voluptatum.<br>
+                    <span>12:15</span>
+                </p>
+            </div>
+            <div class="msg msg-frnd">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, accusamus recusandae iusto
+                    inventore esse quae. <br>
+                    <span>12:15</span>
+                </p>
+            </div>
+        </div>
+
+        <div class="chat-input">
+            <ion-icon name="happy-outline"></ion-icon>
+            <ion-icon name="attach-outline"></ion-icon>
+            <input type="text" placeholder="Type a messsage">
+            <ion-icon name="mic-outline"></ion-icon>
+        </div>
+    </div>
+</div>
+
+
+<!-- icons -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<!-- JavaScript Files -->
+<script src="/js/customer/customer-messages.js"></script>
+<!-- Overlay for the confirmation message -->
+<div id="signOutOverlay" class="overlay">
+    <div class="overlay-content">
+        <p>Are you sure you want to sign out?</p>
+        <button id="confirmSignOut" class="btn"><a href="/technician-logout"></a> Yes</button>
+        <button id="cancelSignOut" class="btn">No</button>
+    </div>
+</div>
+<script src="/js/customer/overlay.js"></script>
+
+</body>
+</html>
+
