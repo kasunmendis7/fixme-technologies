0d376d6971370c210e98b6cd96d942ee61318376
kasunmendis7 <tkomendis@gmail.com>
2024-11-29 09:03:22 +0530

    added admin css styling

--- views/admin/reports.php
+++ views/admin/reports.php
@@ -7,14 +7,55 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Reports</title>
         <link rel="stylesheet" href="/css/admin/admin-dashboard.css">
+        <link rel="stylesheet" href="/css/admin/reports.css">
     </head>
     <body>
     <?php
     include_once 'components/sidebar.php';
     include_once 'components/header.php';
     ?>
-    <!-- <script src="/js/admin/main.js"></script> -->
-   
+    <div class="promotion-container">
+    <div class="promotion-form">
+        <h2>Create Report</h2>
+        <form>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Enter username" required />
+            </div>
+            
+            <div class="form-group">
+                <label for="role">Role</label>
+                <select id="role" name="role">
+                    <option>Select Role</option>
+                    <option>Customer</option>
+                    <option>Technician</option>
+                    <option>Service Centre</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="start-date">Start Date</label>
+                <input type="date" id="start-date" name="start-date" />
+            </div>
+            <div class="form-group">
+                <label for="end-date">End Date</label>
+                <input type="date" id="end-date" name="end-date" />
+            </div>
+            <div class="form-group">
+                <button type="submit">Generate Report</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+    
+    
+    
+    
+    <script src="/js/admin/main.js"></script>
+    
+    </body>
+
+
     
 
                 
