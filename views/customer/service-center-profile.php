c2b0f6ee482fc469c11767f36fdb24f15a54f2f3
sheanemario <sheanemario7@gmail.com>
2025-04-18 11:52:42 +0530

    Add button to get directions to the service center

--- views/customer/service-center-profile.php
+++ views/customer/service-center-profile.php
@@ -47,8 +47,7 @@ include_once 'components/header.php';
                 <span class="status-dot"></span>
                 <span>Available</span>
             </div>
-            <button class="message-btn">Message</button>
-            <button class="message-btn">Call</button>
+            <button class="message-btn">Directions</button>
             <button class="message-btn"
                     onclick="sendRequest( <?php echo $serviceCenter['ser_cen_id'] . ', ' . Application::$app->session->get('customer') ?> )"
             >
