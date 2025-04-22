6205e65a567fead671fb1b42522b21278a3a5787
sheanemario <sheanemario7@gmail.com>
2025-04-22 23:27:47 +0530

    added views for finished contracts

--- views/customer/customer-finished-contracts.php
+++ views/customer/customer-finished-contracts.php
@@ -1,5 +1,7 @@
 <?php
 
+/** @var $finishedContracts */
+
 use app\core\Application;
 
 ?>
@@ -14,6 +16,7 @@ use app\core\Application;
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Customer Finished Contracts</title>
     <link rel="stylesheet" href="/css/customer/customer-dashboard.css">
+    <link rel="stylesheet" href="/css/customer/customer-finished-contracts.css">
     <link rel="stylesheet" href="/css/customer/overlay.css">
     <link rel="stylesheet" href="/css/customer/flash-messages.css">
 </head>
@@ -24,6 +27,46 @@ include_once 'components/sidebar.php';
 include_once 'components/header.php';
 ?>
 
+<div class="details">
+    <div class="activeContracts">
+        <div class="cardHeader">
+            <h2 class="heading">Finished Contracts</h2>
+        </div>
+
+        <table class="modern-table active-contracts-table">
+            <thead>
+            <tr>
+                <th>Contract ID</th>
+                <th>Technician Image</th>
+                <th>Technician Name</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php if (!empty($finishedContracts)): ?>
+                <?php foreach ($finishedContracts as $contract): ?>
+                    <tr>
+                        <td><?= htmlspecialchars($contract['contract_id']) ?></td>
+                        <td>
+                            <img src="<?= htmlspecialchars($contract['profile_picture']) ?>" alt="Techinician Image"
+                                 class="customer-image"/>
+                        </td>
+                        <td><?= htmlspecialchars($contract['technician_name']) ?></td>
+                        <td>
+                            <a href="/customer-finished-contract-details/<?= $contract['contract_id'] ?>"
+                               class="open-contract-btn">View Contract</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="4" style="text-align: center;">No Finished Contracts</td>
+                </tr>
+            <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
 
 <!-- Overlay for the confirmation message -->
 <div id="signOutOverlay" class="overlay">
@@ -37,7 +80,6 @@ include_once 'components/header.php';
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 <script src="/js/customer/overlay.js"></script>
-<script type="text/javascript" src="https://www.payhere.lk/lib/payhere.js"></script>
 </body>
 
 </html>
